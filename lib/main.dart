1d84af75959c06219ac574888bc4e6e4aa17e77b
SELLAMI Kadhem <kadhemsellami@outlook.com>
2023-05-04 13:27:36 +0100

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'app/config/routes/app_pages.dart';
+import 'app/config/themes/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Project Management',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.basic,
+      initialRoute: AppPages.initial,
+      getPages: AppPages.routes,
+    );
+  }
+}
