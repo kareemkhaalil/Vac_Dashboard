0f5d4eedc6ef01a1cf579824419594a5772e5bc0
karem <karem2003.kk@gmail.com>
2023-06-09 21:52:06 +0300

    v0.1

--- lib/screens/auth/sign_up.dart
+++ lib/screens/auth/sign_up.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project_management/bloc/cubits/auth/cubit/auth_cubit.dart';
 import 'package:project_management/utils/theme/images.dart';
+import 'package:project_management/widgets/glass_alert.dart';
 import 'package:project_management/widgets/glass_button.dart';
 import 'package:project_management/widgets/glass_container.dart';
 import 'package:project_management/widgets/glass_textFormField.dart';
@@ -22,8 +23,7 @@ class SignUpScreen extends StatelessWidget {
   final VoidCallback? goToSignInPage;
   final double? height;
   final double? heightUnit;
-  final TextEditingController? passwordController = TextEditingController();
-  final TextEditingController? userNameController = TextEditingController();
+
   final double? width;
   final double? widthUnit;
 
@@ -35,142 +35,142 @@ class SignUpScreen extends StatelessWidget {
         builder: (context, state) {
           final cubit = AuthCubit();
           return Center(
-            child: GlassmorphismContainer(
-              shadowOffset: 20,
-              shadowOpacity: 0.3,
-              blurRadius: 200,
-              shadow: false,
-              height: height! * 0.7,
-              width: width! * 0.6,
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: width! * 0.02,
-                    ),
-                    Column(
+            child: Stack(
+              children: [
+                GlassmorphismContainer(
+                  shadowOffset: 20,
+                  shadowOpacity: 0.3,
+                  blurRadius: 200,
+                  shadow: false,
+                  height: height! * 0.7,
+                  width: width! * 0.6,
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        GlassmorphismTextFormField(
-                          height: height! * 0.05,
-                          width: width! * 0.25,
-                          obscureText: false,
-                          controller: userNameController!,
-                          hintText: "User Name",
-                        ),
                         SizedBox(
-                          height: height! * 0.04,
+                          width: width! * 0.02,
                         ),
-                        GlassmorphismTextFormField(
-                          height: height! * 0.05,
-                          width: width! * 0.25,
-                          obscureText: false,
-                          controller: emailController!,
-                          hintText: "Email",
-                        ),
-                        SizedBox(
-                          height: height! * 0.04,
-                        ),
-                        GlassmorphismTextFormField(
-                          height: height! * 0.05,
-                          width: width! * 0.25,
-                          obscureText: true,
-                          controller: passwordController!,
-                          hintText: "Password",
-                        ),
-                        SizedBox(
-                          height: height! * 0.04,
-                        ),
-                        GlassmorphismButton(
-                          height: height! * 0.05,
-                          width: width! * 0.25,
-                          text: "Sign Up",
-                          onPressed: () {
-                            cubit.signUp(
-                              emailController!.text,
-                              passwordController!.text,
-                              userNameController!.text,
-                            );
-                          },
-                        ),
-                        SizedBox(
-                          height: height! * 0.02,
-                        ),
-                        Row(
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text(
-                              "Already have an account?",
-                              style: TextStyle(
-                                color: Colors.white60,
-                                fontSize: 15,
-                                fontFamily: "Raleway",
-                                fontWeight: FontWeight.w300,
-                              ),
+                            GlassmorphismTextFormField(
+                              height: height! * 0.05,
+                              width: width! * 0.25,
+                              obscureText: false,
+                              controller: cubit.userNameController!,
+                              hintText: "User Name",
                             ),
                             SizedBox(
-                              width: height! * 0.001,
+                              height: height! * 0.04,
                             ),
-                            TextButton(
-                              onPressed: goToSignInPage,
-                              child: Text(
-                                'Sign In',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15,
-                                  fontFamily: "Raleway",
-                                  fontWeight: FontWeight.w700,
+                            GlassmorphismTextFormField(
+                              height: height! * 0.05,
+                              width: width! * 0.25,
+                              obscureText: false,
+                              controller: cubit.emailController!,
+                              hintText: "Email",
+                            ),
+                            SizedBox(
+                              height: height! * 0.04,
+                            ),
+                            GlassmorphismTextFormField(
+                              height: height! * 0.05,
+                              width: width! * 0.25,
+                              obscureText: true,
+                              controller: cubit.passwordController!,
+                              hintText: "Password",
+                            ),
+                            SizedBox(
+                              height: height! * 0.04,
+                            ),
+                            GlassmorphismButton(
+                              height: height! * 0.05,
+                              width: width! * 0.25,
+                              text: "Sign Up",
+                              onPressed: () {
+                                cubit.signUp();
+                              },
+                            ),
+                            SizedBox(
+                              height: height! * 0.02,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "Already have an account?",
+                                  style: TextStyle(
+                                    color: Colors.white60,
+                                    fontSize: 15,
+                                    fontFamily: "Raleway",
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: height! * 0.001,
+                                ),
+                                TextButton(
+                                  onPressed: goToSignInPage,
+                                  child: Text(
+                                    'Sign In',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                      fontFamily: "Raleway",
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: width! * 0.05,
+                        ),
+                        Container(
+                          width: 1,
+                          height: height! * 0.2,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: width! * 0.02,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: height! * 0.2,
+                              width: width! * 0.2,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    AppImages.loginImage,
+                                  ),
+                                  fit: BoxFit.contain,
                                 ),
                               ),
                             ),
-                          ],
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      width: width! * 0.05,
-                    ),
-                    Container(
-                      width: 1,
-                      height: height! * 0.2,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: width! * 0.02,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: height! * 0.2,
-                          width: width! * 0.2,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage(
-                                AppImages.loginImage,
-                              ),
-                              fit: BoxFit.contain,
+                            SizedBox(
+                              height: height! * 0.02,
                             ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: height! * 0.02,
-                        ),
-                        Text(
-                          "Sign Up",
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontFamily: "Raleway",
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white70,
-                              letterSpacing: 1.1),
+                            Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: "Raleway",
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white70,
+                                  letterSpacing: 1.1),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           );
         },
